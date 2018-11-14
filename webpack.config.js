f6b1bc49281554ecacf877e2fee3b0678137a530
Jason Wong <jsnwong@ucdavis.edu>
2018-11-14 12:04:03 -0800

    initial changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  entry: "./frontend/entry.js",
+  entry: "./frontend/portfolio.jsx",
   output: {
     filename: "./frontend/bundle.js"
   },
   devtool: 'source-map',
+  resolve: {
+    extentsions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+    }]
+  }
 };
